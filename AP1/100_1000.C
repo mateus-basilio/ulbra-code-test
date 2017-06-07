b534f882da46426194909d748b13a2992067cdb2
unknown <mateus-basilio2011@hotmail.com>
2017-06-07 17:15:05 -0300

    100_1000

---
+++ AP1/100_1000.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+main( )
+{
+      int n1
+    printf("digite um numero:" );
+    	scanf(%i, &n1);
+    	if((n1>99)(n1<1001))
+    	printf("esse numero está entre 100 e 1000");
+    	else
+    	printf("esse numero não esta entre 100 e 1000");
+
+getche( )
+}
